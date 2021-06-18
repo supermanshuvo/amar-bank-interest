00b3de2d9c4f166eccb5cceeee2936b689e6db0b
supermanshuvo <shuvothesuperman@gmail.com>
2021-06-19 01:33:34 +0600

    Add some input fild for input data and show result

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,95 @@
 <template>
   <v-app>
     <Nav />
+    <v-container>
+        <v-row>
+            <v-col cols="3">
+                <v-subheader>Starting Date : </v-subheader>
+            </v-col>
+            <v-col cols="6">
+                <v-text-field :value="date" slot="activator" label="Starting Date : "></v-text-field>
+                <v-date-picker v-model="date"></v-date-picker>
+            </v-col>
+        </v-row>
+        <v-row>
+            <v-col cols="3">
+                <v-subheader>DPS Amount</v-subheader>
+            </v-col>
+            <v-col cols="6">
+                <v-text-field
+                label="Amount"
+                value="10.00"
+                prefix="$"
+                outlined
+                ></v-text-field>
+            </v-col>
+        </v-row>
+        <v-row>
+            <v-col cols="3">
+                <v-subheader>Annual Interest Rate: </v-subheader>
+            </v-col>
+            <v-col cols="6">
+                <v-text-field
+                label="Amount"
+                value="10.00"
+                prefix="$"
+                outlined
+                ></v-text-field>
+            </v-col>
+        </v-row>
+        <v-row>
+            <v-col cols="3">
+                <v-subheader>No. Of Months :</v-subheader>
+            </v-col>
+            <v-col cols="6">
+                <v-text-field
+                label="Amount"
+                value="10.00"
+                prefix="$"
+                outlined
+                ></v-text-field>
+            </v-col>
+        </v-row>
+        <v-row>
+            <v-col cols="3">
+                <v-subheader>Principal Amount :</v-subheader>
+            </v-col>
+            <v-col cols="6">
+                <v-text-field
+                label="Amount"
+                value="10.00"
+                prefix="$"
+                outlined
+                ></v-text-field>
+            </v-col>
+        </v-row>
+        <v-row>
+            <v-col cols="3">
+                <v-subheader>Interest Amount :</v-subheader>
+            </v-col>
+            <v-col cols="6">
+                <v-text-field
+                label="Amount"
+                value="10.00"
+                prefix="$"
+                outlined
+                ></v-text-field>
+            </v-col>
+        </v-row>
+        <v-row>
+            <v-col cols="3">
+                <v-subheader>Total Amount :</v-subheader>
+            </v-col>
+            <v-col cols="6">
+                <v-text-field
+                label="Amount"
+                value="10.00"
+                prefix="$"
+                outlined
+                ></v-text-field>
+            </v-col>
+        </v-row>
+    </v-container>
   </v-app>
 </template>
 
@@ -11,6 +100,11 @@ export default {
   name: 'Home',
   components: {
     Nav
+  },
+  data(){
+      return{
+          date:''
+      }
   }
 }
 </script>
